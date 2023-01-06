31428f73ea6704c1b5123df5473a480ba4e619f9
go_projects_og <go.TIIT.ogle@gmail.com>
2023-01-06 17:46:11 +0300

    2.14.41

--- client/conponents/Menu/index.js
+++ client/conponents/Menu/index.js
@@ -9,7 +9,7 @@ export const Menu = () => {
       <Link href={'/login'}>Вход</Link>
       <Link href={'/signup'}>Регистрация</Link>
       <Link href={'/admin'}>Admin</Link>
-      <Link href={'https://js-course.ru/courses/show/62b1824805964a1386560a8e'}>Lesson 54:05</Link>
+      <Link href={'https://js-course.ru/courses/show/62b1824805964a1386560a8e'}>Lesson 2.14.41</Link>
     </div>
   );
 };
