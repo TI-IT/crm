ebaef5dda3c44fa94fbf994654d70448e4e1061f
go_projects_og <go.TIIT.ogle@gmail.com>
2023-01-16 00:54:51 +0300

    form clients

--- client/crm/clients/form/index.js
+++ client/crm/clients/form/index.js
@@ -39,13 +39,7 @@ export default function TableaddClients({ server_host }) {
   async function addClients() {
     setDisabled(true);
     setMessage('');
-    if (
-      !clients.name ||
-      !clients.phone ||
-      !clients.organization ||
-      !clients.city ||
-      !clients.address
-    ) {
+    if (!clients.name || !clients.phone || !clients.organization || !clients.city) {
       setMessage('Заполните нужные поля поля');
       setDisabled(false);
       return;
@@ -62,7 +56,7 @@ export default function TableaddClients({ server_host }) {
     const data = await res.json();
     if (data.ok) {
       setMessage('Клиент добавлен');
-      // loadClients(); //*************************** */
+      // loadClient(); //*************************** */
       setDisabled(false);
       setClients({
         surname: '',
@@ -84,155 +78,157 @@ export default function TableaddClients({ server_host }) {
 
   return (
     <>
-      <div>
-        <h2>Добавить Клиента</h2>
-      </div>
+      <h3 className={styles.center}>Добавить Клиента</h3>
+      <div className={styles.center}>{message}</div>
       <div className={styles.form}>
-        <div>{message}</div>
-        <div className={styles.group}>
-          <div className={styles.labelLeft}>
-            <label>{titles.surname}</label>
+        <div className={styles.form1}>
+          <div className={styles.group}>
+            <div className={styles.labelLeft}>
+              <label>{titles.surname}</label>
+            </div>
+            <div className={styles.inputRight}>
+              <input
+                type={'text'}
+                name={'surname'}
+                placeholder={'Фамилия'}
+                onChange={(e) => changeClients('surname', e.target.value)}
+                value={clients.surname}
+              ></input>
+            </div>
           </div>
-          <div className={styles.inputRight}>
-            <input
-              type={'text'}
-              name={'surname'}
-              placeholder={'Фамилия'}
-              onChange={(e) => changeClients('surname', e.target.value)}
-              value={clients.surname}
-            ></input>
+          <div className={styles.group}>
+            <div className={styles.labelLeft}>
+              <label>{titles.name}</label>
+            </div>
+            <div className={styles.inputRight}>
+              <input
+                className={styles.red}
+                type={'text'}
+                name={'name'}
+                placeholder={'Имя'}
+                onChange={(e) => changeClients('name', e.target.value)}
+                value={clients.name}
+              ></input>
+            </div>
           </div>
-        </div>
-        <div className={styles.group}>
-          <div className={styles.labelLeft}>
-            <label>{titles.name}</label>
+          <div className={styles.group}>
+            <div className={styles.labelLeft}>
+              <label>{titles.patronymic}</label>
+            </div>
+            <div className={styles.inputRight}>
+              <input
+                type={'text'}
+                name={'patronymic'}
+                placeholder={'Отчество'}
+                onChange={(e) => changeClients('patronymic', e.target.value)}
+                value={clients.patronymic}
+              ></input>
+            </div>
           </div>
-          <div className={styles.inputRight}>
-            <input
-              className={styles.red}
-              type={'text'}
-              name={'name'}
-              placeholder={'Имя'}
-              onChange={(e) => changeClients('name', e.target.value)}
-              value={clients.name}
-            ></input>
+          <div className={styles.group}>
+            <div className={styles.labelLeft}>
+              <label>{titles.phone}</label>
+            </div>
+            <div className={styles.inputRight}>
+              <input
+                className={styles.red}
+                type={'number'}
+                name={'phone'}
+                placeholder={'Телефон'}
+                onChange={(e) => changeClients('phone', e.target.value)}
+                value={clients.phone}
+              ></input>
+            </div>
           </div>
-        </div>
-        <div className={styles.group}>
-          <div className={styles.labelLeft}>
-            <label>{titles.patronymic}</label>
-          </div>
-          <div className={styles.inputRight}>
-            <input
-              type={'text'}
-              name={'patronymic'}
-              placeholder={'Отчество'}
-              onChange={(e) => changeClients('patronymic', e.target.value)}
-              value={clients.patronymic}
-            ></input>
-          </div>
-        </div>
-        <div className={styles.group}>
-          <div className={styles.labelLeft}>
-            <label>{titles.phone}</label>
-          </div>
-          <div className={styles.inputRight}>
-            <input
-              className={styles.red}
-              type={'number'}
-              name={'phone'}
-              placeholder={'Телефон'}
-              onChange={(e) => changeClients('phone', e.target.value)}
-              value={clients.phone}
-            ></input>
-          </div>
-        </div>
-        <div className={styles.group}>
-          <div className={styles.labelLeft}>
-            <label>{titles.email}</label>
-          </div>
-          <div className={styles.inputRight}>
-            <input
-              className={styles.red}
-              type={'text'}
-              name={'email'}
-              placeholder={'Email'}
-              onChange={(e) => changeClients('email', e.target.value)}
-              value={clients.email}
-            ></input>
-          </div>
-        </div>
-        <div className={styles.group}>
-          <div className={styles.labelLeft}>
-            <label>{titles.organization}</label>
-          </div>
-          <div className={styles.inputRight}>
-            <input
-              className={styles.textarea}
-              type={'text'}
-              name={'organization'}
-              placeholder={'Организация'}
-              onChange={(e) => changeClients('organization', e.target.value)}
-              value={clients.organization}
-            ></input>
+          <div className={styles.group}>
+            <div className={styles.labelLeft}>
+              <label>{titles.email}</label>
+            </div>
+            <div className={styles.inputRight}>
+              <input
+                type={'text'}
+                name={'email'}
+                placeholder={'Email'}
+                onChange={(e) => changeClients('email', e.target.value)}
+                value={clients.email}
+              ></input>
+            </div>
           </div>
         </div>
-        <div className={styles.group}>
-          <div className={styles.labelLeft}>
-            <label>{titles.city}</label>
+
+        <div className={styles.form2}>
+          <div className={styles.group}>
+            <div className={styles.labelLeft}>
+              <label>{titles.organization}</label>
+            </div>
+            <div className={styles.inputRight}>
+              <input
+                className={styles.red}
+                type={'text'}
+                name={'organization'}
+                placeholder={'Организация'}
+                onChange={(e) => changeClients('organization', e.target.value)}
+                value={clients.organization}
+              ></input>
+            </div>
           </div>
-          <div className={styles.inputRight}>
-            <input
-              className={styles.textarea}
-              type={'text'}
-              name={'city'}
-              placeholder={'Город'}
-              onChange={(e) => changeClients('city', e.target.value)}
-              value={clients.city}
-            ></input>
+          <div className={styles.group}>
+            <div className={styles.labelLeft}>
+              <label>{titles.city}</label>
+            </div>
+            <div className={styles.inputRight}>
+              <input
+                className={styles.red}
+                type={'text'}
+                name={'city'}
+                placeholder={'Город'}
+                onChange={(e) => changeClients('city', e.target.value)}
+                value={clients.city}
+              ></input>
+            </div>
+          </div>
+          <div className={styles.group}>
+            <div className={styles.labelLeft}>
+              <label>{titles.address}</label>
+            </div>
+            <div className={styles.inputRight}>
+              <textarea
+                className={styles.textarea}
+                type={'text'}
+                name={'address'}
+                placeholder={'Адрес'}
+                onChange={(e) => changeClients('address', e.target.value)}
+                value={clients.address}
+              ></textarea>
+            </div>
+          </div>
+          <div className={styles.group}>
+            <div className={styles.labelLeft}>
+              <label>{titles.notes}</label>
+            </div>
+            <div className={styles.inputRight}>
+              <textarea
+                className={styles.textarea}
+                type={'text'}
+                name={'notes'}
+                placeholder={'Примечания'}
+                onChange={(e) => changeClients('notes', e.target.value)}
+                value={clients.notes}
+              ></textarea>
+            </div>
           </div>
         </div>
-        <div className={styles.group}>
-          <div className={styles.labelLeft}>
-            <label>{titles.address}</label>
-          </div>
-          <div className={styles.inputRight}>
-            <textarea
-              className={styles.textarea}
-              type={'text'}
-              name={'address'}
-              placeholder={'Адрес'}
-              onChange={(e) => changeClients('address', e.target.value)}
-              value={clients.address}
-            ></textarea>
-          </div>
-        </div>
-        <div className={styles.group}>
-          <div className={styles.labelLeft}>
-            <label>{titles.notes}</label>
-          </div>
-          <div className={styles.inputRight}>
-            <textarea
-              className={styles.textarea}
-              type={'text'}
-              name={'notes'}
-              placeholder={'Примечания'}
-              onChange={(e) => changeClients('notes', e.target.value)}
-              value={clients.notes}
-            ></textarea>
-          </div>
-        </div>
-        <div className={styles.center}>
-          <div>
-            <button
-              type={'button'}
-              onClick={addClients}
-              className={styles.button}
-              disabled={disabled}
-            >
-              Добавить
-            </button>
-          </div>
+      </div>
+      <div className={styles.center}>
+        <div>
+          <button
+            className={styles.button}
+            type={'button'}
+            onClick={addClients}
+            disabled={disabled}
+          >
+            Добавить
+          </button>
         </div>
       </div>
     </>
--- client/crm/clients/page/index.js
+++ client/crm/clients/page/index.js
@@ -4,7 +4,6 @@ import TableaddClients from '../form';
 export default function ClientsPage({ server_host }) {
   return (
     <>
-      <h2>Страница Клиента</h2>
       <TableaddClients server_host={server_host} />
     </>
   );
