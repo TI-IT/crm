8371e33adebeb939e1332a093b5db3a66291f47f
go_projects_og <go.TIIT.ogle@gmail.com>
2023-01-17 00:53:30 +0300

    grid

--- client/crm/applications/form/index.js
+++ client/crm/applications/form/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import styles from './Form.module.scss';
+import FormGroupSelect from '../components/formGroupSelect';
 
 export default function ApplicationsForm({ server_host }) {
   const [citys, setCitys] = React.useState([]);
   const [addCity, setAddCity] = React.useState({});
   const [hide, sethide] = React.useState(styles.hide);
   const [titles, setTitles] = React.useState({
-    surname: 'Фамилия',
+    client: 'Клиент',
     name: 'Имя',
     patronymic: 'Отчество',
     phone: 'Телефон',
@@ -17,7 +18,7 @@ export default function ApplicationsForm({ server_host }) {
     address: 'Адрес',
     notes: 'Примечания',
   });
-  const [clients, setClients] = React.useState({
+  const [newApplications, setNewApplications] = React.useState({
     surname: '',
     name: '',
     patronymic: '',
@@ -28,13 +29,26 @@ export default function ApplicationsForm({ server_host }) {
     address: '',
     notes: '',
   });
+  const [clients, setClients] = React.useState([
+    {
+      surname: '',
+      name: '',
+      patronymic: '',
+      phone: '',
+      email: '',
+      organization: '',
+      city: '',
+      address: '',
+      notes: '',
+    },
+  ]);
   const [message, setMessage] = React.useState('');
   const [disabled, setDisabled] = React.useState(false);
   const router = useRouter('/');
 
-  function changeClients(name, value) {
-    setClients({
-      ...clients,
+  function changeNewApplications(name, value) {
+    setNewApplications({
+      ...newApplications,
       [name]: value,
     });
   }
@@ -66,7 +80,7 @@ export default function ApplicationsForm({ server_host }) {
   async function addClients() {
     setDisabled(true);
     setMessage('');
-    if (!clients.name || !clients.phone || !clients.organization) {
+    if (!newApplications.name || !newApplications.phone || !newApplications.organization) {
       setMessage('Заполните нужные поля поля');
       setDisabled(false);
       return;
@@ -95,7 +109,7 @@ export default function ApplicationsForm({ server_host }) {
         address: '',
         notes: '',
       });
-      router.push('/applications');
+      // router.push('/applications');
     } else {
       setDisabled(false);
       setMessage('Ошибка попробуйте другие данные');
@@ -135,187 +149,56 @@ export default function ApplicationsForm({ server_host }) {
 
   return (
     <>
-      <h3 className={styles.center}>Добавить Клиента</h3>
-      <div className={styles.center}>{message}</div>
-      <div className={styles.form}>
-        <div className={styles.form1}>
-          <div className={styles.group}>
-            <div className={styles.labelLeft}>
-              <label>{titles.surname}</label>
-            </div>
-            <div className={styles.inputRight}>
-              <input
-                type={'text'}
-                name={'surname'}
-                placeholder={'Фамилия'}
-                onChange={(e) => changeClients('surname', e.target.value)}
-                value={clients.surname}
-                onClick={displayHide}
-              ></input>
-            </div>
-          </div>
-          <div className={styles.group}>
-            <div className={styles.labelLeft}>
-              <label>{titles.name}</label>
-            </div>
-            <div className={styles.inputRight}>
-              <input
-                className={styles.red}
-                type={'text'}
-                name={'name'}
-                placeholder={'Имя'}
-                onChange={(e) => changeClients('name', e.target.value)}
-                value={clients.name}
-                onClick={displayHide}
-              ></input>
-            </div>
-          </div>
-          <div className={styles.group}>
-            <div className={styles.labelLeft}>
-              <label>{titles.patronymic}</label>
-            </div>
-            <div className={styles.inputRight}>
-              <input
-                type={'text'}
-                name={'patronymic'}
-                placeholder={'Отчество'}
-                onChange={(e) => changeClients('patronymic', e.target.value)}
-                value={clients.patronymic}
-                onClick={displayHide}
-              ></input>
-            </div>
-          </div>
-          <div className={styles.group}>
-            <div className={styles.labelLeft}>
-              <label>{titles.phone}</label>
-            </div>
-            <div className={styles.inputRight}>
-              <input
-                className={styles.red}
-                type={'number'}
-                name={'phone'}
-                placeholder={'Телефон'}
-                onChange={(e) => changeClients('phone', e.target.value)}
-                value={clients.phone}
-                onClick={displayHide}
-              ></input>
-            </div>
-          </div>
-          <div className={styles.group}>
-            <div className={styles.labelLeft}>
-              <label>{titles.email}</label>
-            </div>
-            <div className={styles.inputRight}>
-              <input
-                type={'text'}
-                name={'email'}
-                placeholder={'Email'}
-                onChange={(e) => changeClients('email', e.target.value)}
-                value={clients.email}
-                onClick={displayHide}
-              ></input>
-            </div>
-          </div>
+      <div className={styles.gridInput}>
+        <div className={styles.gridInputText}>
+          <input placeholder={'Input'} />
         </div>
-
-        <div className={styles.form2}>
-          <div className={styles.group}>
-            <div className={styles.labelLeft}>
-              <label>{titles.organization}</label>
-            </div>
-            <div className={styles.inputRight}>
-              <input
-                className={styles.red}
-                type={'text'}
-                name={'organization'}
-                placeholder={'Организация'}
-                onChange={(e) => changeClients('organization', e.target.value)}
-                value={clients.organization}
-                onClick={displayHide}
-              ></input>
-            </div>
-          </div>
-          <div className={styles.group}>
-            <div className={styles.labelLeft}>
-              <label>{titles.city}</label>
-            </div>
-            <div className={styles.inputRight}>
-              <select
-                className={styles.select}
-                onChange={(e) => changeClients('city', e.target.value)}
-              >
-                <option>{''}</option>
-                {citys.map((city, id) => (
-                  <option key={id}>{city}</option>
-                ))}
-              </select>
-              <div className={styles.buttonGroup}>
-                <button className={styles.button} type={'button'} onClick={displayShow}>
-                  +
-                </button>
-                <div className={hide}>
-                  <div className={styles.GroupChildren}>
-                    <input
-                      type={'text'}
-                      name={'city'}
-                      placeholder={'Город'}
-                      onChange={(e) => changeCity('city', e.target.value)}
-                      value={addCity.city}
-                    ></input>
-                    <div className={styles.GroupChildrenButton}>
-                      <button className={styles.button} type={'button'} onClick={directoryAddCity}>
-                        добавить
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.group}>
-            <div className={styles.labelLeft}>
-              <label>{titles.address}</label>
-            </div>
-            <div className={styles.inputRight}>
-              <textarea
-                className={styles.textarea}
-                type={'text'}
-                name={'address'}
-                placeholder={'Адрес'}
-                onChange={(e) => changeClients('address', e.target.value)}
-                value={clients.address}
-                onClick={displayHide}
-              ></textarea>
-            </div>
-          </div>
-          <div className={styles.group}>
-            <div className={styles.labelLeft}>
-              <label>{titles.notes}</label>
-            </div>
-            <div className={styles.inputRight}>
-              <textarea
-                className={styles.textarea}
-                type={'text'}
-                name={'notes'}
-                placeholder={'Примечания'}
-                onChange={(e) => changeClients('notes', e.target.value)}
-                value={clients.notes}
-                onClick={displayHide}
-              ></textarea>
-            </div>
-          </div>
+        <div className={styles.gridInputButton}>
+          <button>Button</button>
         </div>
       </div>
-      <div className={styles.center}>
-        <div>
-          <button
-            className={styles.button}
-            type={'button'}
-            onClick={addClients}
-            disabled={disabled}
-          >
-            Добавить
-          </button>
+      <div className={styles.card}>
+        <div className={styles.bigCard}>
+          <div className={styles.gridInputButton}>
+            <span>
+              <input placeholder={'Input'} />
+            </span>
+            <div>
+              <button>Button</button>
+            </div>
+          </div>
+          <div className={styles.gridInput}>
+            <span>
+              <input placeholder={'Input'} />
+            </span>
+          </div>
+          <div className={styles.miniCard}>
+            <div className={styles.gridInput}>
+              <span>
+                <input placeholder={'Input'} />
+              </span>
+            </div>
+            <div className={styles.gridInput}>
+              <span>
+                <input placeholder={'Input'} />
+              </span>
+            </div>
+            <div className={styles.gridInput}>
+              <span>
+                <input placeholder={'Input'} />
+              </span>
+            </div>
+            <div className={styles.gridInput}>
+              <span>
+                <input placeholder={'Input'} />
+              </span>
+            </div>
+            <div className={styles.gridInput}>
+              <span>
+                <input placeholder={'Input'} />
+              </span>
+            </div>
+          </div>
         </div>
       </div>
     </>
